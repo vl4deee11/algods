8c39cd6d1e7ab35167ea0e9a77717589cba3a8d3
vl4deee11 <44677024+vl4deee11@users.noreply.github.com>
2021-08-27 16:24:23 +0300

    Create set.go

---
+++ set.go
@@ -0,0 +1,22 @@
+package set
+
+import "testing"
+
+// declaration of memory optimized set: type Set map[T]struct{}
+
+// IntSet ex: integer set with 0 bytes for value
+type set map[int]struct{}
+
+func Test_IntSet(t *testing.T) {
+	s := set{}
+	for i := 0; i < 50000; i++ {
+		s[i] = struct{}{}
+	}
+	if _, ok := s[11]; !ok {
+		panic("key 11 is not present")
+	}
+
+	if _, ok := s[50001]; ok {
+		panic("key 50001 is present")
+	}
+}
