4ab58d5871274a478a139e441d3415cd545065ee
v.boychenko <v.boychenko@vk.team>
2022-11-25 14:13:07 +0400

    added

--- tree/huffman_code_tree_test.go
+++ tree/huffman_code_tree_test.go
@@ -2,6 +2,7 @@ package tree
 
 import (
 	"container/heap"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,6 +19,27 @@ type node struct {
 	c int
 	// b байт символа в который в листе
 	b byte
+}
+
+// code - восстанавливает префикс код хаффмана, если текущий узел - правый ребенок родителя,
+// устанавливаем бит в 1, иначе в ноль
+func (n *node) code() (uint64, byte) {
+	cp := n.p
+	var (
+		r    uint64
+		bits byte
+	)
+	for cp != nil {
+		if cp.r == n {
+			r |= 1 << bits
+		}
+		bits++
+
+		n = cp
+		cp = cp.p
+	}
+
+	return r, bits
 }
 
 // Куча для построения дерева Хаффмана
@@ -44,23 +66,26 @@ func (h *MinHeap) Pop() interface{} {
 //	                     (c: 10)
 //	(c:4, b: 'a')                         (c:6)
 //	                       (c:3, b: 'c')                  (c:3)
-//	                                         (c:2, b: 'b')     (c:1, b: 'd')
-func Build(str string) *node {
+//	                                         (c:2, b: 'd')     (c:1, b: 'b')
+func Build(str string) (*node, map[byte]*node) {
 	// Считаем частоты в строке
 	hm := make(map[byte]int)
 	for i := range str {
 		hm[str[i]]++
 	}
 
+	b2n := make(map[byte]*node)
 	leaves := make([]*node, 0, len(hm))
 	for b := range hm {
-		leaves = append(leaves, &node{c: hm[b], b: b})
+		l := &node{c: hm[b], b: b}
+		b2n[b] = l
+		leaves = append(leaves, l)
 	}
 
 	// Инициализируем кучу для алгоритма
 	h := MinHeap(leaves)
 	heap.Init(&h)
-	return buildTree(&h)
+	return buildTree(&h), b2n
 }
 
 // buildTree - строит дерево хаффмана.
@@ -85,9 +110,22 @@ func buildTree(h *MinHeap) *node {
 
 func TestHuffmanCodeTree(t *testing.T) {
 	str := "aaaabbcccd"
-	tr := Build(str)
+	tr, b2n := Build(str)
 	assert.Equal(t, tr.l.b, byte('a'))
 	assert.Equal(t, tr.r.l.b, byte('c'))
 	assert.Equal(t, tr.r.r.r.b, byte('b'))
 	assert.Equal(t, tr.r.r.l.b, byte('d'))
+
+	r, _ := b2n['a'].code()
+	assert.Equal(t, fmt.Sprintf("%b", r), "0")
+
+	r, _ = b2n['b'].code()
+	assert.Equal(t, fmt.Sprintf("%b", r), "111")
+
+	r, _ = b2n['c'].code()
+	assert.Equal(t, fmt.Sprintf("%b", r), "10")
+
+	r, _ = b2n['d'].code()
+	assert.Equal(t, fmt.Sprintf("%b", r), "110")
+
 }
