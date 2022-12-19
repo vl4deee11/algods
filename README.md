87143f22fd716b3793b34162e71c9c9502d1e0f2
v.boychenko <v.boychenko@vk.team>
2022-12-19 12:51:32 +0400

    added cutpoint

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # algods
-
+Основные материалы собранные в этом репозитории взяты с сайта [e-maxx.ru](http://e-maxx.ru), переписаны на golang.
+Данный репозиторий служит шпаргалкой на тренировках по CP, а так же неким наглядным пособием для лекций по алгоритмам
 # optimization tricks
   1. use sync.Pool https://pkg.go.dev/sync#Pool
   2. use b2s or s2b for converting bytes to string or string to bytes
@@ -8,7 +9,6 @@
   5. build with - `go build -gcflags='-l -l -l' example.go` - to make aggressive inlines (also can use `-l -l` and dont use `-l`)
   6. https://habr.com/ru/company/badoo/blog/301990/
 # more
-+ add ds AVL tree 
-+ add ds fenwick tree https://e-maxx.ru/algo/fenwick_tree
++ add ds AVL tree
 + add algo prim http://e-maxx.ru/algo/mst_prim
 + add algo kruskal http://e-maxx.ru/algo/mst_kruskal_with_dsu
--- graph/cut_vertex_test.go
+++ graph/cut_vertex_test.go
@@ -8,27 +8,47 @@ var minTime []int
 var u []bool
 var g map[int][]int
 
-func dfsC(v int, p int) {
+// dfsCutPoint - за время O(n+m)
+// Пусть дан связный неориентированный граф.
+// Точкой сочленения (или точкой артикуляции, англ. "cut vertex" или "articulation point") называется такая вершина,
+// удаление которой делает граф несвязным.
+func dfsCutPoint(v int, p int) {
+	// Ставим метку того, что уже посещали вершину
 	u[v] = true
+	// Увеличиваем время захода при поиске
 	t++
+	// Время входа в вершину
 	enterTime[v] = t
+	// Время minTime[v] равно минимуму из
+	// enterTime[v] - времени захода в саму вершину,
+	// enterTime[p] - времён захода в каждую вершину p, являющуюся концом некоторого обратного ребра (v, p),
+	// minTime[to]  - времён для каждой вершины to, являющейся непосредственным сыном v в дереве поиска
+	// minTime - является рекурсивным минимум
 	minTime[v] = t
 	childrens := 0
 	for i := range g[v] {
 		nv := g[v][i]
 		if nv == p {
+			// Это критерий прохода по ребру дерева поиска в обратную сторону, мы его не обрабатываем
 			continue
 		}
 
 		if u[nv] {
+			// Если вершину nv мы уже обрабатывали ранее, то обновим minTime нашей текущей вершины v,
+			// таким образом мы словим обратное ребро
 			if enterTime[nv] < minTime[v] {
 				minTime[v] = enterTime[nv]
 			}
 		} else {
-			dfsC(nv, v)
+			// Иначе, вершину nv мы еще не обрабатывали
+			dfsCutPoint(nv, v)
+			// Обновим minTime нашей текущей вершины v
 			if minTime[nv] < minTime[v] {
 				minTime[v] = minTime[nv]
 			}
+			// p != -1, тут сразу отсекаем корень дерева
+			// Это условие главное для точки сочленения - звучит так, если есть ребенок у вершины у которого minTime[nv] >= enterTime[v],
+			// то узел v является точкой сочленения, и не существует обратного ребра из nv в v
 			if minTime[nv] >= enterTime[v] && p != -1 {
 				// cutpoint
 				fmt.Println(v)
@@ -37,14 +57,18 @@ func dfsC(v int, p int) {
 		}
 
 	}
+	// Рассмотрим теперь оставшийся случай: v = root.
+	// Тогда эта вершина является точкой сочленения тогда и только тогда, когда эта вершина имеет более одного сына в дереве обхода в глубину.
+	// В самом деле, это означает, что, пройдя из root по произвольному ребру, мы не смогли обойти весь граф,
+	// откуда сразу следует, что root — точка сочленения.
+	// Если бы мы прошли весть граф в глубину, то childrens == 1 или childrens == 0
 	if p == -1 && childrens > 1 {
 		// cutpoint
 		fmt.Println(v)
 	}
 }
 
-// findCutPoint - find cutpoint in graph
-// link to docs - https://e-maxx.ru/algo/cutpoints
+// findCutPoint - Ищет точки сочленения в графе
 func findCutPoint(n int, connections [][]int) {
 	t = 0
 	enterTime = make([]int, n)
@@ -58,5 +82,6 @@ func findCutPoint(n int, connections [][]int) {
 	for i := 0; i < n; i++ {
 		u[i] = false
 	}
-	dfsC(0, -1)
+	// В отличие от поиска мостов, мы всегда идем в один узел
+	dfsCutPoint(0, -1)
 }
