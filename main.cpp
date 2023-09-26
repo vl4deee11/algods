0e46a48268066a47b51c114b75620cecf196709f
v.boichenko <v.boichenko@uzum.com>
2023-09-26 10:48:17 +0400

    upd cpp templ

--- main.cpp
+++ main.cpp
@@ -5,23 +5,34 @@
 #include <cmath>
 #include <vector>
 #include <map>
+#include <string>
+#include <sstream>
 using namespace std;
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
 /* TYPES  */
 #define ll long long
 #define pii pair<int, int>
 #define pll pair<long long, long long>
 #define vi vector<int>
+#define vs vector<string>
 #define vll vector<long long>
 #define mii map<int, int>
 #define si set<int>
 #define sc set<char>
 
 /* FUNCTIONS */
-#define f(i,s,e) for(long long int i=s;i<e;i++)
-#define cf(i,s,e) for(long long int i=s;i<=e;i++)
-#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define v(t) vector<t>
+#define s(t) set<t>
+#define m(t,t2) map<t, t2>
+#define f(i, s, e) for(long long int i=s;i<e;i++)
+#define cf(i, s, e) for(long long int i=s;i<=e;i++)
+#define rf(i, e, s) for(long long int i=e-1;i>=s;i--)
+#define wl(c) while(c)
 #define pb push_back
+#define mpp make_pair
+#define cl clear
 #define eb emplace_back
 
 /* PRINTS */
@@ -34,50 +45,39 @@ void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cou
 #define read(type) readInt<type>()
 ll min(ll a,int b) { if (a<b) return a; return b; }
 ll min(int a,ll b) { if (a<b) return a; return b; }
+ll min(ll a,ll b) { if (a<b) return a; return b; }
 ll max(ll a,int b) { if (a>b) return a; return b; }
 ll max(int a,ll b) { if (a>b) return a; return b; }
+ll max(ll a,ll b) { if (a>b) return a; return b; }
+int chaz_to_int026(char x) {return int(x - 'a');}
+int chAZ_to_int026(char x) {return int(x - 'A');}
+char int026_to_chaz(int x) {return char(x + 'a');}
+char int026_to_chAZ(int x) {return char(x + 'A');}
+int mod(int a, int b) {return (a % b + b) % b;}
+bool float64_eq(double f1, double f2) {return abs(f1 - f2) < 1e-6;}
+bool float64_gt_or_eq(double f1, double f2) {return float64_eq(f1, f2) || f1 > f2;}
+bool float64_lt_or_eq(double f1, double f2) {return float64_eq(f1, f2) || f1 < f2;}
+ll sum_ap(ll a, ll b) {ll n = (b - a) + 1;return (n * (a + b)) / 2;}
+double dist(pii p1, pii p2) {return sqrt(pow(double(p2.first - p1.first), 2) + pow(double(p2.second - p1.second), 2));}
 ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
 ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+pair<int, int> chess(string b) {return make_pair(7 - (int(b[0]) - 48) - 1, 7 - (int(b[1]) - 'a'));}
+bool ch(pair<int, int> a, pair<int, int> b, pair<int, int> c) {return int64_t(b.first - a.first) * int64_t(c.second - a.second) - int64_t(b.second - a.second) * int64_t(c.first - a.first) >= 0;}
 string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
 string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
 bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
-
+vector<string> split(const string& s, char delimiter){vector<string> tokens;string token;istringstream tokenStream(s);wl(std::getline(tokenStream, token, delimiter)){tokens.push_back(token);};return tokens;}
 
 /*  All Required define Pre-Processors and typedef Constants */
 typedef long int int32;
 typedef unsigned long int uint32;
 typedef long long int int64;
-typedef unsigned long long int  uint64;
-
-/* Extra */
-int max(int x, int y) {if (x > y) {return x;}return y;}
-int min(int x, int y) {if (x < y) {return x;}return y;}
-int abs(int x) {if (x < 0) {return -x;}return x;}
-int str_to_int(string s) {return std::stoi(s);}
-int chaz_to_int026(char x) {return int(x - 'a');}
-int chAZ_to_int026(char x) {return int(x - 'A');}
-char int026_to_chaz(int x) {return char(x + 'a');}
-char int026_to_chAZ(int x) {return char(x + 'A');}
-bool is_let(char b) {return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z');}
-bool is_n(char b) {return b >= '0' && b <= '9';}
-double dist(pair<int, int> p1, pair<int, int> p2) {return sqrt(pow(double(p2.first - p1.first), 2) + pow(double(p2.second - p1.second), 2));}
-int mod(int a, int b) {return (a % b + b) % b;}
-pair<int, int> chess(string b) {return make_pair(7 - (int(b[0]) - 48) - 1, 7 - (int(b[1]) - 'a'));}
-bool float64_eq(double f1, double f2) {return abs(f1 - f2) < 1e-6;}
-bool float64_gt_or_eq(double f1, double f2) {return float64_eq(f1, f2) || f1 > f2;}
-bool float64_lt_or_eq(double f1, double f2) {return float64_eq(f1, f2) || f1 < f2;}
-bool ch(pair<int, int> a, pair<int, int> b, pair<int, int> c) {return int64_t(b.first - a.first) * int64_t(c.second - a.second) - int64_t(b.second - a.second) * int64_t(c.first - a.first) >= 0;}
-int64_t sum_ap(int64_t a, int64_t b) {int64_t n = (b - a) + 1;return (n * (a + b)) / 2;}
-
+typedef unsigned long long int uint64;
 
 // g++ -std=c++20 -O2 -lm -o x.bin main.cpp && chmod +x ./x.bin | cat i.txt | ./x.bin > o.txt
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-
     return 0;
 }
-
-
-
